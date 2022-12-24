41832ccf64eb2f509358e6c5fd26007953ffe22e
smopuim2 <114752061+smopuim2@users.noreply.github.com>
2022-12-24 17:39:26 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+print("* Python Interpreter\n# Type 'Info' to get info,'Exit' to exit.")
+print(">",end=" ")
+n=(sys.stdin.readline())
+print(n,end="")
+while n!="Exit\n":
+	n=n+"#"
+	for i in range(len(n)):
+		if n[i]=="#":
+			n=n[0:i]
+			break
+	for j in range(len(n)-1,-1,-1):
+		if n[j]==" " or n[j]=="\n" or n[j]=="\t":
+			n=n[0:j]
+		else:
+			break
+	n=n+"\n"
+	if n=="Info\n":
+		print("# A simple python interpreter using EXEC.\n(So it is not very safe)\n# Enter code behind '>'.\n")
+		print(">",end=" ")
+		n=(sys.stdin.readline())
+		print(n,end="")
+	elif len(n)>1 and n[-2]==":":
+		print("# Use a non-block to stop entering.")
+		print(">",end=" ")
+		m=(sys.stdin.readline())
+		print(m,end="")
+		while m[0]=="\t" or (len(m)>3 and m[0:4]=="    "):
+			n=n+m
+			print(">",end=" ")
+			m=(sys.stdin.readline())
+			print(m,end="")
+		print("$",end=" ")
+		exec(n)
+		n=m
+	else:
+		print("$",end=" ")
+		exec(n)
+		print(">",end=" ")
+		n=(sys.stdin.readline())
+		print(n,end="")
+print("# Thanks for using!")
